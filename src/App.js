1d6f6fe8ecf292e323648731a5b06c6ed0a33139
NyashaMashumba <nmashumba5@gmail.com>
2021-10-05 15:01:30 +0200

    intro to react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Login from './components/login/login';
+import Calculator from './components/Calculator/Calculator'
+
+function App() {
+  return (
+    <div className="App">
+      <Login/>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Calculator/Calculator.jsx
@@ -0,0 +1,20 @@
+// import './index.css
+
+import Login from "../login/login"
+
+function Calculator(){
+    return(
+        <div>
+            <h1> This is the Calculator</h1>
+
+            <div>
+                <p> This is the  paragraph</p>
+            </div>
+
+            <Login/>
+
+        </div>
+    )
+}
+
+export default Calculator
