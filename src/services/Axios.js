bb8b83db728329cbaed94343438c7009823a5f44
blackbox <dzzirima@gmail.com>
2022-07-04 01:20:12 +0200

    working on web

--- src/services/Axios.js
+++ src/services/Axios.js
@@ -1,7 +1,8 @@
 import {create} from "axios";
 
 const BaseURL = create({
-    baseURL: 'http://localhost:4000/api/v1'
+    baseURL: 'https://finalyearproject3.herokuapp.com/api/v1'
+    // baseURL: 'http://localhost:4000/api/v1'
   })
 
   export default BaseURL
