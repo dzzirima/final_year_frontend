f0999b85bb9963a76de83b92f226326017849586
blackbox <dzzirima@gmail.com>
2022-07-12 13:45:34 +0200

    gugu funal

--- src/services/Axios.js
+++ src/services/Axios.js
@@ -3,6 +3,7 @@ import {create} from "axios";
 const BaseURL = create({
     baseURL: 'https://gugufinal.herokuapp.com/api/v1'
     // baseURL: 'http://localhost:4000/api/v1'
+    
   })
 
   export default BaseURL
